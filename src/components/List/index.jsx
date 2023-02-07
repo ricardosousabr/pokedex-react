a1dd964cf878851b2d3613bf5432d4c7f91a935d
ricardo <ricardo.br.pi@gmail.com>
2023-02-06 21:27:11 -0300

    feat: create search input and call the API

---
+++ src/components/List/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import axios from "axios";
+
+export default function List() {
+  const [pokemonData, setPokemonData] = useState();
+  const [pokemonName, setPokemonName] = useState()
+  const apiPokemon = "https://pokeapi.co/api/v2/pokemon/" + pokemonName
+
+  function getPokemon() {
+
+    axios.get(apiPokemon)
+    .then(response => {
+      setPokemonData(response.data)
+      console.log(response.data)
+    })
+    .catch(error => console.log(error))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    getPokemon()
+  }
+
+  return(
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => setPokemonName(e.target.value)}/>
+          <button>Search</button>
+        </form>
+      </div>
+      <div>
+        <ul>{pokemonData ? <li>{pokemonData.name}</li> : <li>Escreva o nome do seu pokemon</li>}</ul>
+      </div>
+    </div>
+  )
+}
